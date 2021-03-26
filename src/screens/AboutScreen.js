99afc16bd654c97a6458c9c653ecdf973eb78766
Randal Krebs <randal.t.krebs@gmail.com>
2021-03-26 02:55:13 -0300

    footerIdea

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,6 +1,8 @@
+import Footer2 from "../components/Footer2";
 import Maozinha from "../assets/images/maozinha.png";
 import Matinho from "../assets/images/matinho.png";
 import "./AboutScreen.css";
+import Footer from "../components/Footer";
 // import Mandala from "../assets/images/About/mandala.PNG";
 // import Coelho from "../assets/images/About/coelin.PNG";
 // import Folhas from "../assets/images/About/folhas.PNG";
@@ -45,6 +47,8 @@ const AboutScreen = () => {
           </p>
           <img className="maozinha__img" alt="maozinha" src={Maozinha}></img>
         </div>
+        <Footer2 />
+        <Footer />
       </div>
     </>
   );
